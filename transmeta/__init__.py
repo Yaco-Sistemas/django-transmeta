606dc4c61576453a132cd8b472bce6ba86559744
msaelices@yaco.es <msaelices@yaco.es@f4bd572c-078d-11de-a6e9-d320b29efae0>
2013-08-20 09:29:38 +0000

    New 0.6.11 version with a get_mandatory_fieldname() useful function.
    git-svn-id: http://django-transmeta.googlecode.com/svn/trunk@50 f4bd572c-078d-11de-a6e9-d320b29efae0

--- transmeta/__init__.py
+++ transmeta/__init__.py
@@ -31,6 +31,10 @@ def get_fallback_fieldname(field, lang=None):
     return get_real_fieldname(field, lang=fallback_language())
 
 
+def get_mandatory_fieldname(field, lang=None):
+    return get_real_fieldname(field, lang=mandatory_language())
+
+
 def get_real_fieldname_in_each_language(field):
     return [get_real_fieldname(field, lang[LANGUAGE_CODE])
             for lang in get_languages()]
